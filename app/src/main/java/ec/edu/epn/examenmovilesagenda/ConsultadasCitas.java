5fd96f5f72db44afaf6166c07ed03f08b38f469a
pedro <pedro160710@yahoo.com>
2016-06-07 19:47:07 -0500

    empieza creacion de interfaz consutadaCitaas

--- app/src/main/java/ec/edu/epn/examenmovilesagenda/ConsultadasCitas.java
+++ app/src/main/java/ec/edu/epn/examenmovilesagenda/ConsultadasCitas.java
@@ -2,6 +2,7 @@ package ec.edu.epn.examenmovilesagenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ConsultadasCitas extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class ConsultadasCitas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultadas_citas);
+
+        mostrarCitas();
     }
+
+    public void mostrarCitas(){
+
+
+        
+    }
+
+
 }
