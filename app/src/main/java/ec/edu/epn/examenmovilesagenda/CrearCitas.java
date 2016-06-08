1326bee2bbf6f9c0664038fbccffaf9153767a90
pedro <pedro160710@yahoo.com>
2016-06-07 19:23:58 -0500

    creacion actividades para crear y consultar citas

---
+++ app/src/main/java/ec/edu/epn/examenmovilesagenda/CrearCitas.java
@@ -0,0 +1,13 @@
+package ec.edu.epn.examenmovilesagenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearCitas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_citas);
+    }
+}
