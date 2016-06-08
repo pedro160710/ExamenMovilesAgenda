91be8dc8648ca6106d9a335746d93d605f8e9121
pedro <pedro160710@yahoo.com>
2016-06-07 19:30:31 -0500

    funcion de botones del menu lista

--- app/src/main/java/ec/edu/epn/examenmovilesagenda/MainActivity.java
+++ app/src/main/java/ec/edu/epn/examenmovilesagenda/MainActivity.java
@@ -1,14 +1,34 @@
 package ec.edu.epn.examenmovilesagenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonCrearCitas;
+    private Button buttonConsultadasCitas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonConsultadasCitas = (Button) findViewById(R.id.btnConsultar);
+        buttonCrearCitas = (Button) findViewById(R.id.btnCrear);
+    }
+
+
+    public void crearCitas() {
+        Intent intentCrearCita = new Intent(this, CrearCitas.class);
+        startActivity(intentCrearCita);
+
+    }
+
+
+    public void consultadasCitas() {
+        Intent intentConsultar = new Intent(this, ConsultadasCitas.class);
+        startActivity(intentConsultar);
     }
 //    
 }
